a90dc7d7c269d35f6d33c97f186ca0fa2daf8da1
awef94 <ruban90@inbox.ru>
2021-06-20 17:41:16 +0300

    finaly version
    finaly version

--- 2/jsprogram.js
+++ 2/jsprogram.js
@@ -2,18 +2,20 @@ var first = null;
 var second = null;
 var first2 = null;
 var second2 = null;
+var first3 = null;
+var second3 = null;
 
 function Start() {
 
-while (first2 != "number" || first == NaN) {
+while (first2 != "number" || first3 == true) {
   FirstNum();
   }
-while (second2 != "number" || second == NaN) {
+while (second2 != "number" || second3 == true) {
   SecondNum();
   }
 
   if(first > second){
-    alert('Сасай, ты не у дел :D');
+    alert('Сасай, ты не у дел');
   }
   else if(first < second){
     alert('Красавчик! Бухло и шлюхи твои!');
@@ -26,8 +28,22 @@ while (second2 != "number" || second == NaN) {
 function FirstNum() {
   first = parseInt(prompt('Введите возраст доступа:'));
   first2 = typeof first;
+  first3 = isNaN(first);
+  if(first2 != "number" || first3 == true){
+    console.log('Всё катится в пизду!')
+  }
+  else {
+    console.log('И всё идёт по плану!')
+  }
 }
 function SecondNum() {
   second = parseInt(prompt('Введите свой возраст'));
   second2 = typeof second;
+  second3 = isNaN(second);
+  if(second2 != "number" || second3 == true){
+    console.log('Всё катится в пизду!')
+  }
+  else {
+    console.log('И всё идёт по плану!')
+  }
 }
