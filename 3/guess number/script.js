f2a29947a5c62b5d9ffbeabe673134d03cd71abd
awef94 <ruban90@inbox.ru>
2021-06-24 11:05:26 +0300

    full ver prog

--- 3/guess number/script.js
+++ 3/guess number/script.js
@@ -1,4 +1,5 @@
 let randomNum = parseInt(Math.random() * (10 - 0 + 1) + 0);
+let counter = 0;
 console.log(randomNum);
 
 function Game(){
@@ -10,18 +11,26 @@ if(buttonInner == 'Попробовать!'){
   if(guessNum > randomNum && guessNum != ''){
       document.getElementById('message').innerHTML='Меньше бери, лошара!';
       document.getElementById('try').value='';
+      counter++;
+      document.getElementById('counter').innerHTML=`Попыток: ${counter}`;
     }
   else if(guessNum < randomNum && guessNum != ''){
       document.getElementById('message').innerHTML='Больше бери, даун!';
       document.getElementById('try').value='';
+      counter++;
+      document.getElementById('counter').innerHTML=`Попыток: ${counter}`;
     }
   else if(guessNum == ''){
     document.getElementById('message').innerHTML='Введи число, дебич!';
+    counter++;
+    document.getElementById('counter').innerHTML=`Попыток: ${counter}`;
   }
   else{
       document.getElementById('message').innerHTML='Красава, в точку!';
       document.getElementById('trybutton').innerHTML='Новая игра';
       document.getElementById('trybutton').value='Новая игра';
+      counter++;
+      document.getElementById('counter').innerHTML=`Попыток: ${counter}`;
       }
     }
   else if(buttonInner == 'Новая игра'){
@@ -31,5 +40,7 @@ if(buttonInner == 'Попробовать!'){
     document.getElementById('trybutton').innerHTML='Попробовать!';
     document.getElementById('message').innerHTML='Начинай!'
     document.getElementById('trybutton').value='Попробовать!';
+    counter = 0;
+    document.getElementById('counter').innerHTML=`Попыток: ${counter}`;
   }
 }
